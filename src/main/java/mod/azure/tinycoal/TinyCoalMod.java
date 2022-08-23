c3543ec398632f951463be94c258b427d2b41dbb
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-08-23 17:27:27 -0400

    1.18

--- src/main/java/mod/azure/tinycoal/TinyCoalMod.java
+++ src/main/java/mod/azure/tinycoal/TinyCoalMod.java
@@ -1,22 +1,20 @@
 package mod.azure.tinycoal;
 
 import mod.azure.tinycoal.item.TinyCoalItem;
-import net.minecraft.item.Item;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod("tinycoal")
 public class TinyCoalMod {
 
 	public static final String MODID = "tinycoal";
-
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
-			TinyCoalMod.MODID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 	public static final RegistryObject<Item> TINY_COAL = ITEMS.register("tinycoal", () -> new TinyCoalItem());
 	public static final RegistryObject<Item> TINY_CHARCOAL = ITEMS.register("tinycharcoal", () -> new TinyCoalItem());
 
